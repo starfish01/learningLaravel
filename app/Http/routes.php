8a127fde5de710036d4a20f109ed6f8fe9ffda74
starfish01 <patrick.labes@gmail.com>
2018-05-10 20:46:22 +1000

    learning routes and views

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,20 +16,29 @@ Route::get('/', function () {
 //return "sup";
 });
 
-Route::get('/about', function () {
-    return "about Page";
-});
+//Route::get('/about', function () {
+////    return "about Page";
+////});
+////
+////Route::get('/contact', function () {
+////    return "contact page";
+////});
+////
+////Route::get('/post/{id}/{name}', function($id, $name){
+////   return  "this is post number " . $id . " " . $name;
+////});
+////
+////Route::get('admin/posts/example', array('as'=>'admin.home' , function(){
+////    $url = route('admin.home');
+////
+////    return "this url is " . $url;
+////}));
+///
+///
 
-Route::get('/contact', function () {
-    return "contact page";
-});
+//Route::get('/post/{id}','PostsController@index');
 
-Route::get('/post/{id}/{name}', function($id, $name){
-   return  "this is post number " . $id . " " . $name;
-});
+//Route::resource('posts','PostsController');
+Route::get('/contact','PostsController@contact');
 
-Route::get('admin/posts/example', array('as'=>'admin.home' , function(){
-    $url = route('admin.home');
-
-    return "this url is " . $url;
-}));
+Route::get('post/{id}/{name}','PostsController@show_post');
