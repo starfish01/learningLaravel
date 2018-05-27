bc7acd58238f9a9593ff93fe714ec6504ead287d
starfish01 <patrick.labes@gmail.com>
2018-05-27 10:51:42 +1000

    Weekend Lessons - learning more relationship stuff

--- manytomany/app/User.php
+++ manytomany/app/User.php
@@ -23,10 +23,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-
-    public function posts(){
-        return $this->hasMany('App\Post');
-    }
-
 }
--- onetomany/app/Http/routes.php
+++ onetomany/app/Http/routes.php
@@ -11,6 +11,49 @@
 |
 */
 
+use App\User;
+use App\Post;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/create', function (){
+
+    $user = User::findOrFail(1);
+
+    $post = new Post(['title'=>'myfirstpost','text'=>'adding some text here']);
+
+    $user->posts()->save($post);
+
+});
+
+Route::get('/read', function (){
+
+    $user = User::findOrFail(1);
+
+
+    foreach ($user->posts as $post){
+        echo $post->title . "<br>";
+    }
+
+});
+
+Route::get('/update', function (){
+
+    $user = User::findOrFail(1);
+
+    $user->posts()->where('id', '=', 2)->update(['title'=>'New Title Yo', 'text'=>'new text bitches']);
+
+
+});
+
+Route::get('/delete', function(){
+
+    $user = User::findOrFail(1);
+
+    $user->posts()->whereId(2)->delete();
+
+
 });
