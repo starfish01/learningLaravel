7c57e4db37e5fbad0e52796c3a6d8e32aca9b743
starfish01 <patrick.labes@gmail.com>
2018-09-11 21:57:27 +1000

    proceeding with laravel 5.4 needed to make some adjustments but should work now

--- codehacking1/routes/web.php
+++ codehacking1/routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function (){
+    return view('admin.index');
+});
+
+Route::resource('admin/users', 'AdminUsersController');
+
